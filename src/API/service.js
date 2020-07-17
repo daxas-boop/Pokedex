647616310610970485309093f3dde56e65c52bf3
daxas-boop <daxas.swd@hotmail.com>
2020-07-16 23:53:48 -0300

    Delete service.js

--- src/API/service.js
+++
@@ -1,8 +0,0 @@
-export default function obtenerPokemones(URL = 'https://pokeapi.co/api/v2/pokemon/') {
-  return fetch(URL)
-    .then((pokemones) => pokemones.json())
-    .then((pokemones) => pokemones)
-    .catch(() => {
-      alert('El pokemon no se encontró');
-    });
-}
