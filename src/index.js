a70648aaf5a03c818dc5105c107afde0af761903
daxas-boop <daxas.swd@hotmail.com>
2020-07-16 23:54:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import {
-  mostrarCartaPokemon,
-  mostrarCargandoCard,
   mostrarListaPokemones,
   mostrarCargandoLista,
-} from './ui/ui.js';
+// eslint-disable-next-line import/extensions
+} from './ui/lista.js';
 
-import obtenerPokemones from './API/service.js';
+import {
+  mostrarCargandoCard,
+  mostrarCartaPokemon,
+// eslint-disable-next-line import/extensions
+} from './ui/carta.js';
 
-function mostrarPokemonClickeado() {
-  const $pokemones = document.querySelectorAll('td');
-  $pokemones.forEach(($pokemon) => {
-    $pokemon.addEventListener('click', async () => {
-      mostrarCargandoCard();
-      const pokemon = await obtenerPokemones(`https://pokeapi.co/api/v2/pokemon/${$pokemon.dataset.pokemon}`);
-      if (pokemon !== undefined) {
-        mostrarCartaPokemon(pokemon);
-      }
-    });
-  });
-}
+import {
+  obtenerPokemones,
+  obtenerPokemon,
+// eslint-disable-next-line import/extensions
+} from './servicios/service.js';
 
 async function validarSearchBar(searchBarInput) {
   if (/[^A-Za-z0-9]+/g.test(searchBarInput)) {
+    // eslint-disable-next-line no-alert
     alert('Ingrese solo numeros y letras');
   } if (searchBarInput === '') {
+    // eslint-disable-next-line no-alert
     alert('Ingrese un numero o el nombre de un pokemon');
   } else {
-    const pokemon = await obtenerPokemones(`https://pokeapi.co/api/v2/pokemon/${searchBarInput}`);
+    const pokemon = await obtenerPokemon(searchBarInput);
     if (pokemon !== undefined) {
       mostrarCartaPokemon(pokemon);
     }
@@ -47,67 +45,60 @@ function botonBuscar() {
   });
 }
 
-let URLSiguiente;
-let URLAnterior;
-
-function actualizarBoton(pokemones) {
-  URLSiguiente = pokemones.next;
-  URLAnterior = pokemones.previous;
-  if (URLAnterior === null) {
-    document.querySelector('#botonAnterior').classList.add('disabled');
-  } else {
-    document.querySelector('#botonAnterior').classList.remove('disabled');
-  }
-  if (URLSiguiente === null) {
-    document.querySelector('#botonSiguiente').classList.add('disabled');
-  } else {
-    document.querySelector('#botonSiguiente').classList.remove('disabled');
-  }
-}
-
-async function actualizar(URL) {
-  mostrarCargandoLista();
-  const pokemones = await obtenerPokemones(URL);
-  mostrarListaPokemones(pokemones);
-  actualizarBoton(pokemones);
-  mostrarPokemonClickeado();
-}
-
-function configurarBotones(pokemones) {
-  URLSiguiente = pokemones.next;
-  URLAnterior = pokemones.previous;
-  const $siguiente = document.querySelector('#siguiente');
-  const $anterior = document.querySelector('#anterior');
-
-  $anterior.addEventListener('click', () => {
-    if (URLAnterior === null) {
-      document.querySelector('#botonAnterior').classList.add('disabled');
-    } if (URLAnterior === 'https://pokeapi.co/api/v2/pokemon/?offset=956&limit=4') {
-      URLAnterior = 'https://pokeapi.co/api/v2/pokemon/?offset=940&limit=20';
-      actualizar(URLAnterior);
-    } else {
-      document.querySelector('#botonAnterior').classList.remove('disabled');
-      actualizar(URLAnterior);
-    }
+async function cambiarPagina(pagina) {
+  const POKEMONES_POR_PAGINA = 20;
+  let paginaActual;
+  let offset;
+  const limit = POKEMONES_POR_PAGINA;
+  offset = POKEMONES_POR_PAGINA * (pagina - 1);
+  paginaActual = pagina;
+  mostrarCargandoLista('Cargando...');
+  const respuesta = await obtenerPokemones(offset, limit);
+  const {
+    next: urlSiguiente,
+    previous: urlAnterior,
+    count: totalPokemones,
+  } = respuesta;
+  mostrarListaPokemones(respuesta, async (nombre) => {
+    mostrarCargandoCard('Cargando...');
+    mostrarCartaPokemon(await obtenerPokemon(nombre));
   });
 
-  $siguiente.addEventListener('click', () => {
-    if (URLSiguiente === null) {
-      document.querySelector('#botonSiguiente').classList.add('disabled');
-    } else {
-      document.querySelector('#botonSiguiente').classList.remove('disabled');
-      actualizar(URLSiguiente);
-    }
-  });
+  configurarAnteriorSiguiente(paginaActual, totalPokemones, POKEMONES_POR_PAGINA);
 }
 
-async function inicialisar() {
+function configurarAnteriorSiguiente(paginaActual, totalPokemones, POKEMONES_POR_PAGINA) {
+  const $siguiente = document.querySelector('#botonSiguiente');
+  const $anterior = document.querySelector('#botonAnterior');
+  const totalPaginas = Math.ceil(totalPokemones / POKEMONES_POR_PAGINA);
+  $siguiente.onclick = () => {
+    if (paginaActual < totalPaginas) {
+      $siguiente.classList.remove('disabled');
+      // eslint-disable-next-line no-param-reassign
+      paginaActual += 1;
+      cambiarPagina(paginaActual);
+      $anterior.classList.remove('disabled');
+    } else {
+      $siguiente.classList.add('disabled');
+    }
+  };
+  $anterior.onclick = () => {
+    if (paginaActual < 2) {
+      $anterior.classList.add('disabled');
+      // eslint-disable-next-line no-param-reassign
+    } else {
+      $anterior.classList.remove('disabled');
+      paginaActual -= 1;
+      cambiarPagina(paginaActual);
+    }
+  };
+}
+
+async function inicializar() {
   botonBuscar();
-  mostrarCargandoLista();
-  const pokemones = await obtenerPokemones();
-  mostrarListaPokemones(pokemones);
-  configurarBotones(pokemones);
-  mostrarPokemonClickeado();
+  return cambiarPagina(1)
+    // eslint-disable-next-line no-console
+    .catch((e) => console.error(e));
 }
 
-inicialisar();
+inicializar();
